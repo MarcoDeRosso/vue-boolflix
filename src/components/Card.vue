da508b3940b3d94425e4e01e42bcbee2c9ec7090
MarcoDeRosso <mcoder@hotmail.it>
2021-07-19 12:08:43 +0200

    flags

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,6 +6,7 @@
                 <h2>{{title}}</h2>
                 <h3>Titolo Originale: {{original_title}}</h3>
                 <h4>Lingua: {{original_language}}</h4>
+                <img class="flag" :src="flagImg(original_language)" alt="flag">
                 <h3>Voto: {{vote_average}}</h3>
             </div>
         </div>
@@ -22,6 +23,11 @@ export default {
         vote_average:Number,
         poster_path:String,
     },
+    methods:{
+        flagImg(flag){
+            return require ("../assets/"+flag+".png");
+        }
+    }
 }
 </script>
 
@@ -29,10 +35,10 @@ export default {
 @import "../style/colors.scss";
     .container-card{
         width: 100%;
-        max-height: 550px;
+        max-height: 500px;
         img{
             width: 100%;
-            max-height: 550px;
+            max-height: 500px;
         }
         &:hover .info-container{
             opacity: 1;
@@ -43,6 +49,9 @@ export default {
             text-align: center;
             background: rgba($color: #000000, $alpha: 0.6);
             opacity: 0;
+        }
+        .flag{
+            width: 30px;
         }
     }
 
